1b468bb8e7fb1660dcf92c3a2c08857e3cfbf6fb
Анна <@LAPTOP-63K8I5TL>
2017-07-13 08:35:33 +0500

    FC

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Task_2
 {
@@ -10,6 +11,13 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
+            StreamReader fs = new StreamReader(@"INPUT.TXT");
+            string [] Str = fs.ReadLine().Split(' ');
+            int n;
+            int a;
+            int b;
+
+
         }
     }
 }
