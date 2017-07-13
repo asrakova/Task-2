f9586e50d46f5395527662c5821b056aaa2d5b8f
Анна <@LAPTOP-63K8I5TL>
2017-07-13 15:28:40 +0500

    Основной алгоритм

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -9,15 +9,29 @@ namespace Task_2
 {
     class Program
     {
+        static ulong Factorial(ulong a, ulong b)
+        {
+            ulong n = a;
+            for (ulong i = a; i < b; i++)
+                n = n * i;
+            return n;
+        }
+
         static void Main(string[] args)
         {
             StreamReader fs = new StreamReader(@"INPUT.TXT");
-            string [] Str = fs.ReadLine().Split(' ');
-            int n;
-            int a;
-            int b;
-
-
+            string[] str = fs.ReadLine().Split(' ');
+            ulong n = ulong.Parse(str[0]);
+            ulong a = ulong.Parse(str[1]);
+            ulong b = ulong.Parse(str[2]);
+            ulong resultA = Factorial(a, a + n) / Factorial(1, n);
+            ulong resultB = Factorial(b, b + n) / Factorial(1, n);
+            ulong result = 0;
+            if (resultA == 0) result = resultB;
+            else if (resultB == 0) result = resultA;
+            else result = resultA * resultB;
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
